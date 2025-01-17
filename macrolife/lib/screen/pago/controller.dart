9d54d9c27617512e37cffa1c37e235ff1e8ea217
OsvaldoGS1 <142460596+OsvaldoGS1@users.noreply.github.com>
2025-01-17 12:44:37 -0600

    Pantalla pago

--- macrolife/lib/screen/pago/controller.dart
+++ macrolife/lib/screen/pago/controller.dart
@@ -1,3 +1,15 @@
 import 'package:get/get.dart';
+import 'package:macrolife/helpers/configuraciones.dart';
 
-class PagoController extends GetxController {}
+class PagoController extends GetxController {
+  final ConfiguracionesController configuraciones =
+      Get.put(ConfiguracionesController());
+
+  RxDouble anualPrice = 0.0.obs;
+  @override
+  void onInit() {
+    anualPrice.value =
+        configuraciones.configuraciones.value.suscripcion?.anual ?? 0.0;
+    super.onInit();
+  }
+}
